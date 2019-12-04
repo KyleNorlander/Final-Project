4539fcb4cf66d2b3a88d331169b010098e39cd8f
KyleNorlander <54556328+KyleNorlander@users.noreply.github.com>
2019-12-04 17:14:05 -0500

    Complete

--- game/Scripts/finish.gd
+++ game/Scripts/finish.gd
@@ -9,7 +9,7 @@ func _ready():
 	pass
 	
 func _process(delta):
-	if get_parent().get_node("Character").keygot == true and get_parent().get_node("Character").fusegot == true:
-		if get_node("RayCast").get_collider():
-			#get_tree().change_scene("res://Scenes/Win.tscn") # WIN SCREEN GOES THERE
-			pass
+	#if get_parent().get_node("Character").keygot == true and get_parent().get_node("Character").fusegot == true:
+	if get_node("RayCast").get_collider():
+		get_tree().change_scene("res://Scenes/Win.tscn") # WIN SCREEN GOES THERE
+		
